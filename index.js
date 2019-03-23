b39e619bf362d10994397e008e0d0b36cf2efc4f
pods13 <alta0815@yandex.ru>
2019-03-24 00:22:31 +0300

    Add basic web part to the app

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ global.__basedir = __dirname;
 
 const registerCustomModules = require('module-alias/register')
 const configEnv = require('dotenv').config();
+const express = require('express');
+
+const app = express();
 
 const dataRefresher = require('@data-refresher');
 
 dataRefresher();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`));
