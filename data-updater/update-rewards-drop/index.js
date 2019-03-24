e6d6ac93f17374dd8b55ebf99e2332b3db7f977a
pods13 <alta0815@yandex.ru>
2019-03-24 18:36:09 +0300

    Gather data for elite onslaught

--- data-updater/update-rewards-drop/index.js
+++ data-updater/update-rewards-drop/index.js
@@ -8,11 +8,14 @@ const PATH_TO_ONSLAUGHT_FOLDER = join(__basedir, process.env.REPO_FOLDER,
 	process.env.DATA_FOLDER, process.env.ONSLAUGHT_FOLDER);
 
 const updateSanctuaryOnslaught = async($) => {
-	const pathToPreviousData = [process.env.NORMAL_ONSLAUGHT_FILENAME]
+	const pathToPreviousData = [process.env.NORMAL_ONSLAUGHT_FILENAME, process.env.ELITE_ONSLAUGHT_FILENAME]
 		.map(filename => join(PATH_TO_ONSLAUGHT_FOLDER, filename));
 
-	const currentData = dropsPageData.getDropsByMissionName($, process.env.NORMAL_ONSLAUGHT_MISSION_NAME);
-	const changed = await updateIfChanged(pathToPreviousData, [currentData]);
+	const normalOnslaughtData = dropsPageData.getDropsByMissionName($, process.env.NORMAL_ONSLAUGHT_MISSION_NAME);
+	const eliteOnslaughtData = dropsPageData.getDropsByMissionName($, process.env.ELITE_ONSLAUGHT_MISSION_NAME);
+	const currentData = [normalOnslaughtData, eliteOnslaughtData];
+
+	const changed = await updateIfChanged(pathToPreviousData, currentData);
 
 	if(changed) {
 		const pathToOnslaughtPost = join(__basedir, process.env.REPO_FOLDER, 
